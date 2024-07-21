0400df87424059a97dad404120fcaced3df81132
Alina-Zhulina <157721023+Alina-Zhulina@users.noreply.github.com>
2024-07-21 20:58:53 +0200

    add component FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,5 @@
 import styles from "./FriendList.module.css";
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <div className={styles.friendItem}>
-      <img src={avatar} alt="Avatar" width="48" className={styles.avatar} />
-      <p className={styles.name}>{name}</p>
-      <p
-        className={`${styles.status} ${isOnline ? styles.online : styles.offline}`}
-      >
-        {isOnline ? "Online" : "Offline"}
-      </p>
-    </div>
-  );
-};
-
+import FriendListItem from "./FriendListItem";
 const FriendList = ({ friends }) => {
   return (
     <ul className={styles.friendList}>
